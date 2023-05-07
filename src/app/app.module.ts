31cbb8fb3e30bebd4048d7c0a72d92b84702f670
namedmichi <j.technology@web.de>
2023-05-08 00:05:12 +0200

    create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
